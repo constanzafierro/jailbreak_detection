390148f463db2e1c5225c2865c8bb57dd001f6b8
cfierro94 <constanza.fierro94@gmail.com>
2025-03-06 14:13:30 +0100

    add last layer per example cosine

--- pipeline/compute_cosines.py
+++ pipeline/compute_cosines.py
@@ -122,7 +122,7 @@ def get_mean_cosine_activations(
             pre_hook_extra[1](from_=i, to=i + batch_size),
         )
         with add_hooks(
-            module_forward_pre_hooks=fwd_pre_hooks + pre_hook_extra,
+            module_forward_pre_hooks=fwd_pre_hooks + [pre_hook_extra],
             module_forward_hooks=[],
         ):
             model(
